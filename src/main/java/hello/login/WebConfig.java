3eb93d57afb56beb8f314e1d288cd0cacb1704dc
ujoneMAC@HOME <03toot28@gmail.com>
2022-04-06 23:01:59 +0900

    Filter start

---
+++ src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,25 @@
+package hello.login;
+
+import hello.login.web.filter.LogFilter;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean logFilterRegistration() {
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new LogFilter());
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.addUrlPatterns("/*");
+
+        return filterFilterRegistrationBean;
+    }
+
+}
